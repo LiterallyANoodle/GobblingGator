c600159941474964cc54cb3b3d2bf06fefa2210d
Matthew Mahan <themaskedkid321@gmail.com>
2024-03-25 20:06:46 -0500

    cleanup

--- Assets/Scripts/LeftFlipper.cs
+++ Assets/Scripts/LeftFlipper.cs
@@ -8,11 +8,6 @@ public class LeftFlipper : MonoBehaviour
 {
 
     private bool isUp = false;
-    private const float turnSpeed = 5f;
-    public int duration = 3;
-    private int counter = 0;
-    private Vector3 downAngle = new Vector3(-90f, 45f, 0f);
-    private Vector3 upAngle = new Vector3(-90f, 0f, 0f);
     private Rigidbody rigidBody;
 
     // signals 
@@ -32,17 +27,6 @@ public class LeftFlipper : MonoBehaviour
 
     void FixedUpdate()
     {
-        // if (isUp) {
-        //     counter += 1;
-        //     counter = Math.Clamp(counter, 0, duration);
-        //     float t = (float)counter / duration;
-        //     this.transform.localEulerAngles = Vector3.Lerp(downAngle, upAngle, t);
-        // } else {
-        //     counter -= 1;
-        //     counter = Math.Clamp(counter, 0, duration);
-        //     float t = (float)counter / duration;
-        //     this.transform.localEulerAngles = Vector3.Lerp(downAngle, upAngle, t);
-        // }
         if (isUp) {
             // add torque, spring joint will reset it
             rigidBody.AddRelativeTorque(new Vector3(0, 0, -100000000), ForceMode.Force);
